6e0a660e0de2c08350430514776af455948c4e37
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-06-09 07:32:51 +0800

    Add stored proc for update employee by id

---
+++ MVCCRUDDB/Stored Procedures/spEmployee_UpdateEmployeeByID.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spEmployee_UpdateEmployeeByID]
+	@ID bigint,
+	-- Only contact no. and email address can be updated for an employee
+	@ContactNo numeric(11),
+	@EmailAddress nvarchar(100)
+AS
+BEGIN 
+	UPDATE [dbo].[Employee]
+	SET [ContactNo] = @ContactNo,
+		[EmailAddress] = @EmailAddress
+	WHERE [ID] = @ID
+END
