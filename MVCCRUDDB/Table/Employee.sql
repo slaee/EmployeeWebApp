8d5dc4b48c2577850d448b4cbbed8d7cd89d2cfb
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-06-09 10:56:18 +0800

    Update Employee table

--- MVCCRUDDB/Table/Employee.sql
+++ MVCCRUDDB/Table/Employee.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Employee]
 (
-	[ID] BIGINT NOT NULL PRIMARY KEY, 
+	[ID] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
     [EmpNo] NVARCHAR(6) NOT NULL UNIQUE, 
     [FirstName] NVARCHAR(15) NOT NULL, 
     [LastName] NVARCHAR(15) NOT NULL, 
     [Birthdate] DATE NOT NULL, 
-    [ContactNo] NUMERIC(11) NOT NULL, 
+    [ContactNo] NVARCHAR(11) NOT NULL, 
     [EmailAddress] NVARCHAR(100) NOT NULL,
 )
