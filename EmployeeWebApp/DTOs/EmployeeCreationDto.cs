6dc30967507afa0ccab477acb8788e976a10c607
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-06-09 10:55:47 +0800

    Add employee creation and updation DTOs

---
+++ EmployeeWebApp/DTOs/EmployeeCreationDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeWebApp.DTOs
+{
+    public class EmployeeCreationDto
+    {
+        [Required(ErrorMessage = "Firstname is required.")]
+        public string FirstName { get; set; }
+        
+        [Required(ErrorMessage = "Lastname is required.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Birthdate is required.")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+        [Required(ErrorMessage = "Contact number is required.")]
+        [RegularExpression(@"^09\d{9}$", ErrorMessage = "Invalid contact number.")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "EmailAddress is required.")]
+        [RegularExpression(
+            @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+            + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+            + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$",
+            ErrorMessage = "The email address is not valid"
+        )]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ EmployeeWebApp/DTOs/EmployeeUpdationDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeWebApp.DTOs
+{
+    public class EmployeeUpdationDto
+    {
+        public long ID { get; set; }
+        [Required(ErrorMessage = "Contact number is required.")]
+        [RegularExpression(@"^09\d{9}$", ErrorMessage = "Invalid contact number.")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "EmailAddress is required.")]
+        [RegularExpression(
+            @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+            + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+            + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$",
+            ErrorMessage = "The email address is not valid"
+        )]
+        public string EmailAddress { get; set; }
+    }
+}
